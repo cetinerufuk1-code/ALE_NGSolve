48607e42d383a63d5d9bed5654e6d79d90210cdb
UfukCetiner <cetinerufuk1@gmail.com>
2026-08-25 13:16:19 +0100

    update readme

---
+++ README.md
@@ -0,0 +1,41 @@
+# Heat Conduction Arbitrary Lagrangian Eulerian (ALE) Form
+
+This repository uses the NGSolve library to solve the heat conduction equations in an Arbitrary Lagrangian–Eulerian (ALE) formulation. Additional implementations linearize each contributing term and reformulate the solution field with respect to the steady-state solution.
+
+## Theory
+
+The weak form heat conduction equations in ALE form are given by the following:
+
+$$\int_{\hat{\Omega}} J \left(
+\frac{\partial \hat{u}}{\partial t}\hat{v}
+- F^{-T}\nabla_{\hat{x}}\hat{u}\frac{\partial \Phi}{\partial t}\hat{v}
++ F^{-T}\nabla_{\hat{x}}\hat{u}F^{-T}\nabla_{\hat{x}}\hat{v}
+\right)\,d\hat{x}
+=
+\int_{\hat{\Omega}} J\hat{f}\hat{v}\,d\hat{x}$$
+
+where F and J are a function of the grid deformation $\Phi$. 
+
+$$F = (I + \nabla \Phi)$$
+$$J =\mathrm{det}(F) = \mathrm{det}(I + \nabla \Phi)$$
+
+The terms within the heat conduction equation are grouped and labeled as follows:
+
+$$ \mathrm{Mass:} \,\,\,\, \int_{\hat{\Omega}}J\frac{\partial \hat{u}}{\partial t}\hat{v} = \int_{\hat{\Omega}}J\frac{u^{n+1}-u^{n}}{\Delta t}\hat{v}\,d\hat{x}$$
+
+$$ \mathrm{Convection:} \,\,\,\, - \int_{\hat{\Omega}}JF^{-T}\nabla_{\hat{x}}\hat{u}\frac{\partial \Phi}{\partial t}\hat{v}\,d\hat{x}$$
+
+$$ \mathrm{Diffusion:} \,\,\,\, \int_{\hat{\Omega}}JF^{-T}\nabla_{\hat{x}}\hat{u}F^{-T}\nabla_{\hat{x}}\hat{v}\,d\hat{x}$$
+
+$$ \mathrm{Source:} \,\,\,\, \int_{\hat{\Omega}}J\hat{f}\hat{v}\,d\hat{x}$$
+
+The mass term is discretized using the backward Euler method, with the solution at each time step obtained through a linear solve.
+
+### Deriving Derivatives wrt to grid deformation
+
+Each term in the heat conduction equation is derivated wrt to the grid deformation and the analytical form of each term is as follows:
+
+$$ \mathrm{Mass:} \,\, \frac{\partial M}{\partial\phi} \delta\phi = \int_{\hat{\Omega}} (\frac{J}{\Delta t})\mathrm{tr}(F^{-1}\nabla(\delta\phi))\hat{u}\cdot\hat{v}\,d\hat{x}$$
+
+
+(more derivatives to follow)
--- src/heat_conduction.py
+++ src/heat_conduction.py
@@ -2,11 +2,13 @@
 
 import ngsolve as ngs
 import numpy as np
-from netgen.occ import Rectangle, Glue, X, Y, MoveTo
+from netgen.occ import Glue
+from netgen.occ import MoveTo
 from netgen.occ import OCCGeometry
-import netgen.gui
+from netgen.occ import Rectangle
+from netgen.occ import X
+from netgen.occ import Y
 from ngsolve.internal import visoptions
-import matplotlib.pyplot as plt
 
 # ================ PARAMETERS =================
 ORDER = 3
@@ -14,15 +16,16 @@ MAX_ELEMENT_SIZE = 0.05
 TIME_STEP = 0.001
 END_TIME = 0.5
 SOURCE_STRENGTH = 100
-EPS = 1e-5 # delta for finite difference
+EPS = 1e-5  # delta for finite difference
+
 
 def generate_mesh(
         order: int,
-        max_element_size: float
+        max_element_size: float,
     ) -> ngs.Mesh:
     """Generates the mesh for the rectangular domain for the heat conduction problem.
 
-        Features a rectangular section in the middle labeled "hot" to 
+        Features a rectangular section in the middle labeled "hot" to
         assign for the source term.
 
     Args:
@@ -33,26 +36,27 @@ def generate_mesh(
     Returns:
         Mesh: The resulting mesh.
     """
-    shell = Rectangle(0.5,0.5).Face()
+    shell = Rectangle(0.5, 0.5).Face()
     shell.faces.name = "solid"
-    shell.edges.Min(X).name  = "left"
+    shell.edges.Min(X).name = "left"
     shell.edges.Max(X).name = "right"
     shell.edges.Min(Y).name = "down"
     shell.edges.Max(Y).name = "up"
 
-    hot = MoveTo(0.25-(0.2*0.5),0.25-(0.2*0.5)).Rectangle(0.2,0.2).Face()
+    hot = MoveTo(0.25 - (0.2 * 0.5), 0.25 - (0.2 * 0.5)).Rectangle(0.2, 0.2).Face()
     hot.faces.name = "hot"
 
     domain = shell - hot
 
-    geo = Glue([domain,hot])
+    geo = Glue([domain, hot])
 
-    mesh = ngs.Mesh(OCCGeometry(geo,dim = 2).GenerateMesh(maxh=max_element_size))
+    mesh = ngs.Mesh(OCCGeometry(geo, dim=2).GenerateMesh(maxh=max_element_size))
     mesh.Curve(order)
 
     return mesh
 
-## Grid Motion Function
+# Grid Motion Function
+
 
 def grid_deformation_quantities(
         mesh: ngs.Mesh,
@@ -85,14 +89,15 @@ def grid_deformation_quantities(
         inverse_deformation_gradient,
     )
 
+
 def mass_integral(
         mesh: ngs.Mesh,
-        grid_deformation : ngs.GridFunction,
-        temperature_trial : ngs.CoefficientFunction,
-        temperature_test : ngs.CoefficientFunction
-    ) -> ngs.comp.SumOfIntegrals:  
-    """ALE heat conduction, mass term,
-    
+        grid_deformation: ngs.GridFunction,
+        temperature_trial: ngs.CoefficientFunction,
+        temperature_test: ngs.CoefficientFunction,
+    ) -> ngs.comp.SumOfIntegrals:
+    """ALE heat conduction, mass term.
+
     Symbolic Integral: ∫ J⋅(u/Δt)⋅v dΩ₀
 
     Args:
@@ -100,24 +105,24 @@ def mass_integral(
         grid_deformation: Deformation '𝚽'.
         temperature_trial: Trial function.
         temperature_test: Test function.
-    
-    Returns:
-        mass_term. 
-    """
 
-    _ , deformation_determinant, _ = (
+    Returns:
+        mass_term.
+    """
+    _, deformation_determinant, _ = (
         grid_deformation_quantities(mesh, grid_deformation))
 
     return (deformation_determinant * ngs.InnerProduct(
         temperature_trial, temperature_test) / TIME_STEP
         ) * ngs.dx
 
+
 def diffusion_integral(
     mesh: ngs.Mesh,
-    grid_deformation : ngs.GridFunction,
-    temperature_trial : ngs.CoefficientFunction,
-    temperature_test : ngs.CoefficientFunction
-) -> ngs.comp.SumOfIntegrals:  
+    grid_deformation: ngs.GridFunction,
+    temperature_trial: ngs.CoefficientFunction,
+    temperature_test: ngs.CoefficientFunction,
+) -> ngs.comp.SumOfIntegrals:
     """ALE heat conduction, diffusion term.
 
     Symbolic integral: ∫ J⋅(F⁻ᵀ∇T)⋅(F⁻ᵀ∇v) dΩ₀
@@ -127,13 +132,12 @@ def diffusion_integral(
         grid_deformation: Deformation '𝚽'.
         temperature_trial: Trial function.
         temperature_test: Test function.
-    
+
     Returns:
         diffusion_term.
     """
-
     _, deformation_determinant, inverse_deformation_gradient = (
-        grid_deformation_quantities(mesh,grid_deformation)
+        grid_deformation_quantities(mesh, grid_deformation)
     )
 
     return (deformation_determinant * ngs.InnerProduct(
@@ -141,12 +145,13 @@ def diffusion_integral(
         (inverse_deformation_gradient.trans * ngs.Grad(temperature_test)))
         ) * ngs.dx
 
+
 def convection_integral(
     mesh: ngs.Mesh,
-    grid_deformation : ngs.GridFunction,
-    grid_deformation_old : ngs.GridFunction,
-    temperature_trial : ngs.CoefficientFunction,
-    temperature_test : ngs.CoefficientFunction
+    grid_deformation: ngs.GridFunction,
+    grid_deformation_old: ngs.GridFunction,
+    temperature_trial: ngs.CoefficientFunction,
+    temperature_test: ngs.CoefficientFunction,
 ) -> ngs.comp.SumOfIntegrals:
     """ALE heat conduction, convection term.
 
@@ -158,12 +163,12 @@ def convection_integral(
         grid_deformation_old : Deformation at previous timestep.
         temperature_trial: Trial function.
         temperature_test: Test function.
-    
+
     Returns:
         convection_term.
     """
     _, deformation_determinant, inverse_deformation_gradient = (
-        grid_deformation_quantities(mesh,grid_deformation)
+        grid_deformation_quantities(mesh, grid_deformation)
     )
 
     return -(deformation_determinant * ngs.InnerProduct(
@@ -171,10 +176,11 @@ def convection_integral(
         (grid_deformation - grid_deformation_old) / TIME_STEP * temperature_test)
         ) * ngs.dx
 
+
 def source_integral(
     mesh: ngs.Mesh,
-    grid_deformation : ngs.GridFunction,
-    temperature_test : ngs.CoefficientFunction
+    grid_deformation: ngs.GridFunction,
+    temperature_test: ngs.CoefficientFunction,
 ) -> ngs.comp.SumOfIntegrals:
     """ALE heat conduction, source term.
 
@@ -183,88 +189,89 @@ def source_integral(
     Args:
         mesh: Current finite element mesh.
         grid_deformation: Deformation '𝚽'.
-        temperature_trial: Trial function.
         temperature_test: Test function.
-    
+
     Returns:
         source_term.
     """
     _, deformation_determinant, _ = (
-    grid_deformation_quantities(mesh,grid_deformation)
+    grid_deformation_quantities(mesh, grid_deformation)
     )
 
     return (deformation_determinant * SOURCE_STRENGTH *
         temperature_test) * ngs.dx("hot")
 
+
 def mass_integral_wrt_deformation(
-        mesh : ngs.Mesh,
-        grid_deformation : ngs.GridFunction, 
+        mesh: ngs.Mesh,
+        grid_deformation: ngs.GridFunction,
         temperature_trial: ngs.GridFunction,
-        temperature_test : ngs.GridFunction,
-        delta_grid : ngs.CoefficientFunction,
+        temperature_test: ngs.GridFunction,
+        delta_grid: ngs.CoefficientFunction,
     ) -> ngs.comp.SumOfIntegrals:
     """Calculates derivative of mass term wrt to the grid deformation.
 
-        Symbolic integral: ∂M/∂𝚽⋅δ𝚽 = ∫ J⋅tr(F⁻¹⋅∇δ𝚽)⋅T⋅v/Δt dΩ₀
+    Symbolic integral: ∂M/∂𝚽⋅δ𝚽 = ∫ J⋅tr(F⁻¹⋅∇δ𝚽)⋅u⋅v/Δt dΩ₀
 
-        Args: 
-            mesh: current finite element mesh.
-            grid_deformation: grid deformation at the current timestep.
-            temperature_trial: Trial function
-            temperature_test: Test function.
-            delta_grid: Change in grid deformation.
-        
-        Returns:
-            mass_integral_wrt_deformation.
+    Args:
+        mesh: current finite element mesh.
+        grid_deformation: grid deformation at the current timestep.
+        temperature_trial: Trial function
+        temperature_test: Test function.
+        delta_grid: Change in grid deformation.
+
+    Returns:
+        mass_integral_wrt_deformation.
     """
     _, deformation_jacobian, inverse_deformation_gradient = (
-    grid_deformation_quantities(mesh,grid_deformation)
+    grid_deformation_quantities(mesh, grid_deformation)
     )
 
     # Analytical Derivative Calculation
     deformation_determinant_derivative = (ngs.Trace(
         deformation_jacobian * inverse_deformation_gradient *
-        ngs.Grad(delta_grid)
+        ngs.Grad(delta_grid),
     ))
 
     return (deformation_determinant_derivative *
-        ngs.InnerProduct(temperature_trial,temperature_test) / TIME_STEP
+        ngs.InnerProduct(temperature_trial, temperature_test) / TIME_STEP
         ) * ngs.dx
+
 
 def main() -> None:
     """"Main setup and time iteration."""
-    mesh = generate_mesh(ORDER,MAX_ELEMENT_SIZE)
-    
+    mesh = generate_mesh(ORDER, MAX_ELEMENT_SIZE)
+
     # Setup Displacement and Temperature Fields
     displacement_space = ngs.VectorH1(mesh, order=ORDER)
     grid_deformation, grid_deformation_old = (
         ngs.GridFunction(displacement_space), ngs.GridFunction(displacement_space))
 
-    temperature_space = ngs.H1(mesh,order = ORDER,dirichlet ="left|right|up|down")
-    (temperature_trial) , (temperature_test) = temperature_space.TnT()
+    temperature_space = ngs.H1(mesh, order=ORDER, dirichlet="left|right|up|down")
+    (temperature_trial), (temperature_test) = temperature_space.TnT()
     temperature, temperature_old = (
         ngs.GridFunction(temperature_space), ngs.GridFunction(temperature_space))
-    
+
     # Define the heat conduction equations in weak form
     lhs = ngs.BilinearForm(temperature_space, symmetric=False)
     rhs = ngs.LinearForm(temperature_space)
 
-    lhs += mass_integral(mesh,grid_deformation,temperature_trial,temperature_test)
-    lhs += diffusion_integral(mesh,grid_deformation,temperature_trial,temperature_test)
-    lhs += convection_integral(mesh,grid_deformation,
-                            grid_deformation_old,temperature_trial,temperature_test)
+    lhs += mass_integral(mesh, grid_deformation, temperature_trial, temperature_test)
+    lhs += diffusion_integral(mesh, grid_deformation, temperature_trial, temperature_test)
+    lhs += convection_integral(mesh, grid_deformation,
+                            grid_deformation_old, temperature_trial, temperature_test)
 
-    rhs += source_integral(mesh,grid_deformation,temperature_test)
-    rhs += mass_integral(mesh,grid_deformation,temperature_old,temperature_test)
-    
+    rhs += source_integral(mesh, grid_deformation, temperature_test)
+    rhs += mass_integral(mesh, grid_deformation, temperature_old, temperature_test)
+
     preconditioner = ngs.Preconditioner(lhs, type="multigrid", inverse="sparsecholesky")
 
-    # Initial Conditions 
-    temperature.Set(0.0) # (this might be unnecessary)
+    # Initial Conditions
+    temperature.Set(0.0)  # (this might be unnecessary)
 
     # Begin visualization
-    ngs.Draw(temperature,mesh,"temperature")
-    ngs.Draw(grid_deformation,mesh,"displacement")
+    ngs.Draw(temperature, mesh, "temperature")
+    ngs.Draw(grid_deformation, mesh, "displacement")
     visoptions.scalfunction = "temperature:0"
     visoptions.vecfunction = "displacement"
     visoptions.deformation = 1
@@ -277,17 +284,19 @@ def main() -> None:
             grid_deformation_old.vec.data = grid_deformation.vec
 
             # Assign some deformation
-            displace_x = 0.035 * ngs.sin(ngs.pi * ngs.x / 0.5 / 2) * ngs.sin(ngs.pi * ngs.y / 0.5) * ngs.sin((10*ngs.pi)*t)
-            displace_y = 0.035 * ngs.sin(ngs.pi * ngs.y / 0.5 / 2) * ngs.sin(ngs.pi * ngs.x / 0.5) * ngs.sin((10*ngs.pi)*t)
+            displace_x = (0.035 * ngs.sin(ngs.pi * ngs.x / 0.5 / 2) *
+                ngs.sin(ngs.pi * ngs.y / 0.5) * ngs.sin((10 * ngs.pi) * t))
+            displace_y = (0.035 * ngs.sin(ngs.pi * ngs.y / 0.5 / 2) *
+                ngs.sin(ngs.pi * ngs.x / 0.5) * ngs.sin((10 * ngs.pi) * t))
 
             grid_deformation.Set(
-                ngs.CF((displace_x,displace_y))
-            ) 
-            
-            # Solve 
+                ngs.CF((displace_x, displace_y)),
+            )
+
+            # Solve
             lhs.Assemble()
             rhs.Assemble()
-            inv = ngs.CGSolver(lhs.mat,preconditioner.mat,printrates=False)
+            inv = ngs.CGSolver(lhs.mat, preconditioner.mat, printrates=False)
             temperature.vec.data = (
                 inv * rhs.vec)
 
@@ -297,9 +306,9 @@ def main() -> None:
             # Calculate Mass Derivative
             # To-Do: Implement function for each derivative
             delta_grid = grid_deformation - grid_deformation_old
-            mass_derivative_analytical = ngs.BilinearForm(temperature_space, symmetric = False)
+            mass_derivative_analytical = ngs.BilinearForm(temperature_space, symmetric=False)
             mass_derivative_analytical += (mass_integral_wrt_deformation(
-                mesh,grid_deformation,temperature_trial,temperature_test,delta_grid)
+                mesh, grid_deformation, temperature_trial, temperature_test, delta_grid)
             )
             mass_derivative_matrix_analytical = mass_derivative_analytical.Assemble().mat
 
@@ -307,9 +316,9 @@ def main() -> None:
             grid_deformation_dd = ngs.GridFunction(displacement_space)
             grid_deformation_dd.Set(grid_deformation + EPS * delta_grid)
 
-            mass_derivative_finite = ngs.BilinearForm(temperature_space, symmetric = False)
-            mass_derivative_finite += (1/EPS) * (
-                mass_integral(mesh,grid_deformation_dd,temperature_trial, temperature_test) -
+            mass_derivative_finite = ngs.BilinearForm(temperature_space, symmetric=False)
+            mass_derivative_finite += (1 / EPS) * (
+                mass_integral(mesh, grid_deformation_dd, temperature_trial, temperature_test) -
                 mass_integral(mesh, grid_deformation, temperature_trial, temperature_test))
 
             mass_derivative_matrix_finite = mass_derivative_finite.Assemble().mat
@@ -318,9 +327,9 @@ def main() -> None:
             _, _, va = mass_derivative_matrix_analytical.COO()
             _, _, vb = mass_derivative_matrix_finite.COO()
 
-            np.testing.assert_allclose(va, vb, rtol = 0, atol=1e-8, strict=False)
+            np.testing.assert_allclose(va, vb, rtol=0, atol=1e-8, strict=False)
 
-            t += TIME_STEP 
+            t += TIME_STEP
 
     input("Press any key to exit...")
 
